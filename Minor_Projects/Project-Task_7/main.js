e6a683a74b6cf5c8d988389ae8b2c674faee3ce2
yyash01 <yjain3.436@gmail.com>
2020-07-25 19:44:46 +0530

    added project-7

---
+++ Minor_Projects/Project-Task_7/main.js
@@ -0,0 +1,11 @@
+var myage = 18;//variable of myage
+var earlyYears =2;
+earlyYears = earlyYears*(10.5);
+var laterYear = myage-2;
+laterYear = laterYear*4;
+myageIndogyear = earlyYears+laterYear;
+var myname = "Yash".toLowerCase();//The toLowerCase method returns a string with
+// all lowercase letters.
+
+
+console.log(`My name is ${myname}. I am ${myage} years old in human years which is ${myageIndogyear} years old in dog years.`);
