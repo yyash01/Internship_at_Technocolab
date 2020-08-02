c70cd3d0b37bb82d55b12ca0477de6dcbfd6964a
yyash01 <yjain3.436@gmail.com>
2020-08-02 13:46:26 +0530

    added with | exporting the functions with change name | steps-11-12-13 of project

---
+++ Minor_Projects/Project-task_19/employee.js
@@ -0,0 +1,46 @@
+export let Employee = new Object();
+
+Employee.salary = 100000;
+
+let payGrades = {
+  entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
+  midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
+  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+};
+
+var getCadre = function() {
+  if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
+    return 'entryLevel';
+  } else if (Employee.salary >= payGrades.midLevel.minSalary && Employee.salary <= payGrades.midLevel.maxSalary) {
+    return 'midLevel';
+  } else return 'seniorLevel';
+}
+
+var calculateTax = function() {
+  return payGrades[getCadre()].taxMultiplier * (Employee.salary);
+}
+
+var getBenefits = function() {
+  return payGrades[getCadre()].benefits.join(', ');
+}
+
+var calculateBonus = function() {
+  return .02 * (Employee.salary);
+}
+
+var reimbursementEligibility = function() {
+  let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
+  let totalBenefitsValue = 0; 
+  let employeeBenefits = payGrades[getCadre()].benefits;
+  for (let i = 0; i < employeeBenefits.length; i++) {
+    totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+  }
+  return totalBenefitsValue;
+}
+export {getCadre as cadre,calculateTax as tax , getBenefits as benefits , calculateBonus as bonus , reimbursementEligibility as reimbursement};
+
+
+
+
+
+
