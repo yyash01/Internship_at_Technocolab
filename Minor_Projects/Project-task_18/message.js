2e22e476a2b881738c8a1ac4732681a115b9436f
yyash01 <yjain3.436@gmail.com>
2020-08-02 13:44:16 +0530

    added project-18 | run it on firefox

---
+++ Minor_Projects/Project-task_18/message.js
@@ -0,0 +1,12 @@
+import { MessageMixer } from './messageMixer.js';
+
+function displayMessage() {
+  console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
+  console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(MessageMixer.reverseWord("What is the color of the sky?"));
+  console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
+  console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(MessageMixer.encode("What is the color of the sky?"));
+}
+
+displayMessage();
---
+++ Minor_Projects/Project-task_18/messageMixer.js
@@ -0,0 +1,53 @@
+export const MessageMixer = {};
+
+MessageMixer.countCharacter = function countCharacter(inputString, inputCharacter) {
+  let count = 0;
+  let string = inputString.toLowerCase();
+  let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === character) {
+         count++;
+      }
+    }
+  return count; 
+};
+
+MessageMixer.capitalizeFirstCharacterOfWords = function capitalizeFirstCharacterOfWords(string) {
+  let arr = string.split(" ");  
+    for (let i = 0; i < arr.length; i++) {  
+      let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1); 
+    }
+  return arr.join(" "); 
+};
+
+
+MessageMixer.reverseWord = function reverseWord(word) {
+  return word.split("").reverse().join("");
+};
+
+MessageMixer.reverseAllWords = function reverseAllWords(sentence) {
+  let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = MessageMixer.reverseWord(words[i]);
+    }
+   return words.join(" ");
+};
+
+
+MessageMixer.replaceFirstOccurence = function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
+  return string.replace(toBeReplaced, replaceWith);
+};
+
+
+MessageMixer.replaceAllOccurrences = function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
+  return string.split(toBeReplaced).join(replaceWith);
+};
+
+MessageMixer.encode = function encode(string) {
+  let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
+    for (let key in replacementObject) {
+      string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]); 
+    } 
+    return string;
+};
