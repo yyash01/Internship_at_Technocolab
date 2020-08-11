a1b9f67cfa5c01c3d627ef5386f9c59bcfc7d378
yyash01 <yjain3.436@gmail.com>
2020-08-11 20:57:03 +0530

    added project-24

---
+++ Minor_Projects/project-task_22/SourceCode/p22/src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Contact from './components/Contact.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
